dae4b5d370fe8b8eeb84a43420c745f0d29de50d
SOHAM BHUNIYA <sohambhuniyaofficial@gmail.com>
2026-04-20 15:04:42 +0530

    fix: add SSL and dynamic CORS for production

--- server/index.js
+++ server/index.js
@@ -4,7 +4,21 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+const allowedOrigins = [
+  'http://localhost:5173',
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
